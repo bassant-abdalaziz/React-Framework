e36aeaafbc2be74bc612bf0fb9e1d207f9156636
Bassant <bassantabdalaziz1999@gmail.com>
2023-09-14 22:07:42 +0300

    Updates

---
+++ static/js/67.c79b3a37.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkframework=self.webpackChunkframework||[]).push([[67],{865:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});t(2791);var a=t.p+"static/media/notFound.94b4abace8acbb8f1a43.png",r={img:"NotFound_img__xeTyB"},c=t(184);function i(){return(0,c.jsx)("div",{className:"d-flex justify-content-center align-items-center mt-5",children:(0,c.jsx)("img",{src:a,alt:"error",className:r.img})})}}}]);
+//# sourceMappingURL=67.c79b3a37.chunk.js.map
